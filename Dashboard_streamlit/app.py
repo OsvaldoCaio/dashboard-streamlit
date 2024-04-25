2363ae18b07914ec8fc4c52d68b5318054c674d3
Osvaldo Caio Oliveira dos Santos <149591131+OsvaldoCaio@users.noreply.github.com>
2024-04-25 18:50:23 -0300

    Update app.py

--- Dashboard_streamlit/app.py
+++ Dashboard_streamlit/app.py
@@ -32,7 +32,7 @@ if aba_selecionada == 'O Desafio':
     paragraphs = [
         "Você foi contratado(a) para uma consultoria, e seu trabalho envolve analisar os dados de preço do petróleo Brent, que pode ser encontrado no site do Ipea.",
         "Essa base de dados histórica envolve duas colunas: data e preço (em dólares).",
-        "Um grande cliente do segmento pediu para que a consultoria desenvolvesse um dashboard interativo e que gere insights relevantes para tomada de decisão.",
+        "Um grande cliente do segmento pediu para que a consultoria desenvolvesse um dashboard interativo e que gere insights relevantes para tomada de decisão.“Oh!” é esse Streamlit!",
         "Além disso, solicitaram que fosse desenvolvido um modelo de Machine Learning para fazer o forecasting do preço do petróleo.",
     ]
     
